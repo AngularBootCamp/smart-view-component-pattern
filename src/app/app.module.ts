a1a35f5736c7d594717080093ee8de6c790559a3
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-05-07 16:24:31 -0500

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { EmployeeDisplayComponent } from './employee-display/employee-display.component';
-import { EmployeeLoader } from './employee-loader.service';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -21,7 +20,6 @@ import { AppRoutingModule } from './app-routing.module';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [EmployeeLoader],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
