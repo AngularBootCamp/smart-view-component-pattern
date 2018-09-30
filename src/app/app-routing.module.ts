6ee270de4418daa3d0fa02166b6882874ca658ec
Paul Spears <paul.spears@oasisdigital.com>
2018-09-30 08:03:20 -0500

    curriculum update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
